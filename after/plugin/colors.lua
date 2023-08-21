d99e9aafc100d969163fd5e6998ab6b6917aa977
Keshav <s.keshav13142@gmail.com>
2023-08-21 12:11:46 +0530

    setup theme before bufferline

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,40 +1,5 @@
 vim.o.background = "dark"
 
-require("gruvbox").setup({
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = true,
-		comments = true,
-		operators = false,
-		folds = true,
-	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = false,
-	contrast = "soft",
-	palette_overrides = {
-		bright_aqua = "#89B482",
-		bright_blue = "#83a598",
-		bright_green = "#A9B665",
-		bright_orange = "#E78A4E",
-		bright_purple = "#d3869b",
-		bright_red = "#EA6962",
-		bright_yellow = "#D8A657",
-		dark0 = "#282828",
-		dark0_soft = "#1b1b1b",
-		gray = "#7C6F64",
-		light1 = "#D4BE98",
-	},
-	dim_inactive = false,
-	transparent_mode = true,
-})
-vim.cmd("colorscheme gruvbox")
-
 -- For neovim and nvim-tree transparency
 require("transparent").setup({
 	extra_groups = {
