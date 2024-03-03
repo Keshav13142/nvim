6fcc387e599c383d8884a6036180e6caf9f0299e
Keshav <s.keshav13142@gmail.com>
2024-03-03 19:07:08 +0530

    update plugins and install venv selector

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -191,4 +191,15 @@ return {
 			},
 		},
 	},
+
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+		opts = {},
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+			{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+		},
+	},
 }
