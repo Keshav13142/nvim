c6c4a068232f4714a438337fb1e03f96a117e6b2
Keshav <s.keshav13142@gmail.com>
2024-04-20 08:32:45 +0530

    add markdown preview plugin

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -202,4 +202,14 @@ return {
 			{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
 		},
 	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
