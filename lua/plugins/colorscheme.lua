48cf9b371254c6de8dbdd9414a808cbe98904819
Keshav <s.keshav13142@gmail.com>
2024-09-14 17:19:40 +0530

    transparency stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -366,22 +366,12 @@ return {
 					"Normal",
 					"NormalFloat",
 					"NormalNC",
-					"NvimTreeEndOfBuffer",
-					"NvimTreeNormal",
-					"NvimTreeNormalFloat",
-					"NvimTreeNormalNC",
 					"SignColumn",
-					"TelescopeBorder",
 				},
 			})
 
-			-- For transparent bufferline
-			vim.g.transparent_groups = vim.list_extend(
-				vim.g.transparent_groups or {},
-				vim.tbl_map(function(v)
-					return v.hl_group
-				end, vim.tbl_values(require("bufferline.config").highlights))
-			)
+			require("transparent").clear_prefix("telescope")
+			require("transparent").clear_prefix("BufferLine")
 		end,
 	},
 
